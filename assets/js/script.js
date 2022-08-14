e8b5bba6711508e399ae16085e53799ec85734be
AngelaSheahan <alouise_35@yahoo.co.uk>
2022-08-14 16:33:46 +0000

    set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+function runGame() {
+
+
+}
+
+function checkAnswer() {
+
+
+}
+
+function CalculateCorrectAnswer () {
+
+
+}
+
+function incrementScore() {
+
+
+}
+
+function incrementWrongAnswer() {
+
+
+}
+
+function displayAdditionQuestion() {
+
+
+}
+
+function displaySubtractQuestion() {
+
+
+}
+
+function displayMultiplyQuestion() {
+
+
+}
+
+function displayDivideQuestion() {
+
+    
+}
+
